242b150b25c66560f5bd880e554e8b366f68eafa
Paul Pessoa <paulmspessoa@gmail.com>
2022-08-24 00:14:32 -0300

    adição de dados nas caracteristicas do usuario

--- src/components/patientTable.vue
+++ src/components/patientTable.vue
@@ -9,52 +9,41 @@
           mollitia!
         </div>
       </v-card-text>
-
+        <!-- Patients search -->
       <v-text-field type="search" outlined label="Searching..." placeholder="Type something..." single-line
         class="mb-4 px-4 pb-4" hide-details dense append-icon="mdi-account-search" @click:append="loadPatients()">
       </v-text-field>
     </v-card>
     <v-card class="pa-4">
       <h1>{{ msg }}</h1>
-      <v-data-table :headers="headers" :items="patients" hide-default-footer :items-per-page="linesTable"
- sort-by="name" :loading="isLoading" class="elevation-1">
-        <!--Dialogo visualizar pacientes-->
+      <v-data-table :headers="headers" :items="patients" hide-default-footer :items-per-page="linesTable" sort-by="name"
+        :loading="isLoading" class="elevation-1">
+        <!-- Detail patients -->
         <template v-slot:top>
-          <v-dialog v-model="dialogDetail" max-width="400px">
+          <v-dialog v-model="dialogDetail" max-width="460px">
             <v-card>
-             <div class="py-4 mb-2">
-                <v-img style="border-radius: 150px" class="mx-auto" alt="Imagem" height="100px" width="100px" :src="patients[0].picture.medium"></v-img>
-
+              <div class="py-4 mb-2">
+                <v-img style="border-radius: 150px" class="mx-auto" alt="Imagem" height="100px" width="100px"
+                  :src="detailsedit.imageMedium"></v-img>
               </div>
               <v-card-text>
-                <v-row>
-                  <v-text-field v-model="patients[0].name.first" label="Name" outlined dense hide-details class="mb-2">
-                  </v-text-field>
-                  <v-text-field v-model="patients[0].email" label="Email" outlined dense hide-details class="mb-2">
-                  </v-text-field>
-                  <v-text-field v-model="patients[0].gender" label="Gender" outlined dense hide-details class="mb-2">
-                  </v-text-field>
-                  <v-text-field v-model="patients[0].dob.date" label="Birth" outlined dense hide-details class="mb-2">
-                  </v-text-field>
-                  <v-text-field v-model="patients[0].phone" label="Phone" outlined dense hide-details class="mb-2">
-                  </v-text-field>
-                  <v-text-field v-model="patients[0].location.country" label="Nationality" outlined dense hide-details
-                    class="mb-2">
-                  </v-text-field>
-                  <v-text-field v-model="patients[0].location.street.name" label="Location" outlined dense hide-details
-                    class="mb-2">
-                  </v-text-field>
-                  <v-text-field v-model="patients[0].id.value" label="ID" outlined dense hide-details class="mb-2">
-                  </v-text-field>
-                </v-row>
+                <v-text-field v-model="detailsedit.fullName" label="Name" outlined dense hide-details class="mb-2" />
+                <v-text-field v-model="detailsedit.email" label="Email" outlined dense hide-details class="mb-2" />
+                <v-text-field v-model="detailsedit.gender" label="Gender" outlined dense hide-details class="mb-2" />
+                <v-text-field v-model="detailsedit.birth" label="Birth" outlined dense hide-details class="mb-2" />
+                <v-text-field v-model="detailsedit.phone" label="Phone" outlined dense hide-details class="mb-2" />
+                <v-text-field v-model="detailsedit.country" label="Nationality" outlined dense hide-details
+                  class="mb-2" />
+                <v-text-field v-model="detailsedit.fullAddress" label="Location" outlined dense hide-details
+                  class="mb-2" />
+                <v-text-field v-model="detailsedit.id" label="ID" outlined dense hide-details class="mb-2" />
               </v-card-text>
-
-              <v-card-actions>
-                <v-btn color="error" text @click="close" class="mb-2">
+              <v-card-actions class="pa-6">
+                <v-btn color="error" text @click="close">
                   Close
                 </v-btn>
                 <v-spacer></v-spacer>
-                <v-btn color="primary" class="mb-2">
+                <v-btn color="primary">
                   <v-icon small class="mr-2">mdi-share-variant</v-icon>
                   Share
                 </v-btn>
@@ -63,29 +52,29 @@
           </v-dialog>
         </template>
 
-        <!--Name Column-->
+        <!--Name column-->
         <template v-slot:[`item.name`]="{ item }">
           <span class="justify-center">
-            {{ item.name.first }}
-            {{ item.name.last }}
+            {{ item.firstName }}
+            {{ item.lastName }}
           </span>
         </template>
 
-        <!--Gender Column -->
+        <!--Gender column -->
         <template v-slot:[`item.gender`]="{ item }">
           <span class="justify-center">
-            {{ item.gender[0].toUpperCase() + item.gender.substring(1) }}
+            {{ item.gender[0]?.toUpperCase() + item.gender.substring(1) || '' }}
           </span>
         </template>
 
-        <!--Birth Column -->
+        <!-- Birth column -->
         <template v-slot:[`item.birth`]="{ item }">
           <span class="justify-center">
-            {{ new Date(item.dob.date).toLocaleDateString() }}
+            {{ item.birth }}
           </span>
         </template>
 
-        <!--Botão para visualizar modal -->
+        <!-- Details patients button -->
         <template v-slot:[`item.actions`]="{ item }">
           <v-btn @click="viewItem(item)" class="primary">
             <v-icon small class="mr-2">
@@ -95,11 +84,12 @@
         </template>
       </v-data-table>
       <v-card-actions>
-        <v-btn :loading="isLoading" class="primary mx-auto px-4" @click="loadPatients()"><v-icon small class="mr-2">mdi-layers-plus</v-icon>Load more</v-btn>
+        <v-btn :loading="isLoading" class="primary mx-auto px-4" @click="loadPatients()">
+          <v-icon small class="mr-2">mdi-layers-plus</v-icon>Load more
+        </v-btn>
       </v-card-actions>
     </v-card>
   </v-container>
-
 </template>
 
 <script>
@@ -109,7 +99,7 @@ export default {
   name: 'patientTable',
   props: ['msg'],
   data: () => ({
-    linesTable: 1,
+    linesTable: 5,
     link: null,
     searhPatients: true,
     isLoading: false,
@@ -119,45 +109,33 @@ export default {
       { text: 'Birth', value: 'birth', align: 'center' },
       { text: 'Actions', value: 'actions', align: 'center', sortable: false },
     ],
-    patients: [{
-      name: {
-        first: null,
-        last: null,
-        title: null
-      },
-      email: null,
-      phone: null,
-      gender: null,
-      dob: {
-        date: null,
-      },
-      picture: {
-        medium: null,
-      },
-      location: {
-        country: null,
-        street: {
-          name: null,
-          number: null,
-        }
-      }
-    }],
+    patients: [],
     detailsedit: {},
     dialogDetail: false
   }),
-  // watch: {
-  //   dialog(val) {
-  //     val || this.close()
-  //   }
-  // },
   methods: {
     listPatients() {
       this.isLoading = true
       axios
         .get(URL_PATIENTS + this.linesTable, {})
         .then((response) => {
-          console.log(response.data.results)
-          this.patients = response.data.results
+          // console.log(response.data.results)
+          this.patients = response.data.results.map(user => ({
+            firstName: user.name.first,
+            lastName: user.name.last,
+            fullName: user.name.first + ' ' + user.name.last,
+            gender: user.gender[0]?.toUpperCase() + user.gender.substring(1) || '',
+            country: user.location.country + ' - ' + user.nat,
+            postcode: user.location.postcode,
+            fullAddress: user.location.street.name + ' ' + user.location.street.number + ', ' + user.location.city + ' - ' + user.location.state + ' - ' + user.location.postcode,
+            latitude: user.location.coordinates.latitude,
+            longitude: user.location.coordinates.longitude,
+            birth: new Date(user.dob.date).toLocaleDateString(),
+            phone: user.phone,
+            email: user.email,
+            imageMedium: user.picture.medium,
+            id: user.login.uuid
+          }));
         })
         .catch((error) => {
           console.log(error)
@@ -183,9 +161,4 @@ export default {
     this.initialize()
   }
 }
-
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-</style>
